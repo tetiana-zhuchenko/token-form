e8c13d710521da62f39a73cf57557d590d3e0857
Tania Zhuchenko <taniyhamail@gmail.com>
2024-07-13 10:59:22 +0300

    fix: fixed localStorage function

--- src/app/helpers/setToLocalStorage.ts
+++ src/app/helpers/setToLocalStorage.ts
@@ -3,11 +3,13 @@
 import { TTokenData } from '../types/TTokenData'
 
 export const setToLocalStorage = (data: TTokenData) => {
-  const existingData = localStorage.getItem('tokApp')
-  let tokens = []
-  if (existingData) {
-    tokens = JSON.parse(existingData)
+  if (typeof window !== 'undefined') {
+    const existingData = localStorage.getItem('tokApp')
+    let tokens = []
+    if (existingData) {
+      tokens = JSON.parse(existingData)
+    }
+    tokens.push(data)
+    localStorage.setItem('tokApp', JSON.stringify(tokens))
   }
-  tokens.push(data)
-  localStorage.setItem('tokApp', JSON.stringify(tokens))
 }
